6c8e769d50097567c2a3c0a009a087ed1d4dca48
Max Graey <maxgraey@gmail.com>
2017-01-15 19:27:48 +0200

    Many improvements and bump version to 1.1.0
    * Add styles for icon and star’s view
    * selectedStar is optional now
    * Optimize render if interaction is disabled
    * Other minor optimizations

--- star-rating.js
+++ star-rating.js
@@ -1,86 +1,145 @@
+'use strict';
+
 // React and react native imports
 import {
   StyleSheet,
   View,
+  Text
 } from 'react-native';
 
 import React, {
   Component,
-  PropTypes,
+  PropTypes
 } from 'react';
 
-// Local file imports
-import StarButton from './star-button';
+// Third party imports
+import Button from 'react-native-button';
+import EntypoIcons from 'react-native-vector-icons/Entypo';
+import EvilIconsIcons from 'react-native-vector-icons/EvilIcons';
+import FontAwesomeIcons from 'react-native-vector-icons/FontAwesome';
+import FoundationIcons from 'react-native-vector-icons/Foundation';
+import IoniconsIcons from 'react-native-vector-icons/Ionicons';
+import MaterialIconsIcons from 'react-native-vector-icons/MaterialIcons';
+import OcticonsIcons from 'react-native-vector-icons/Octicons';
+import ZocialIcons from 'react-native-vector-icons/Zocial';
 
-const styles = StyleSheet.create({
-  starRatingContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
+const iconSets = {
+  Entypo: EntypoIcons,
+  EvilIcons: EvilIconsIcons,
+  FontAwesome: FontAwesomeIcons,
+  Foundation: FoundationIcons,
+  Ionicons: IoniconsIcons,
+  MaterialIcons: MaterialIconsIcons,
+  Octicons: OcticonsIcons,
+  Zocial: ZocialIcons
+};
 
 class StarRating extends Component {
 
   constructor(props) {
     super(props);
 
+    // Round rating down to nearest .5 star
+    const roundedRating = this.round(this.props.rating);
     this.state = {
       maxStars: this.props.maxStars,
+      rating:   this.round(this.props.rating)
     };
-
-    this.onStarButtonPress = this.onStarButtonPress.bind(this);
   }
 
-  onStarButtonPress(rating) {
-    this.props.selectedStar(rating);
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      rating: this.round(nextProps.rating)
+    });
   }
 
   round(number) {
     return (Math.round(number * 2) / 2);
   }
 
-  render() {
-    // Round rating down to nearest .5 star
-    let starsLeft = this.round(this.props.rating);
-    let starButtons = [];
+  pressStarButton(rating) {
+    if (this.props.selectedStar) {
+      this.props.selectedStar(rating);
+    }
+    this.setState({
+      rating: rating
+    });
+  }
 
-    for (let i = 0; i < this.state.maxStars; i++) {
-      let starIconName = this.props.emptyStar;
-      let starColor = this.props.emptyStarColor;
+  render() {
+
+    var {
+        disabled,
+        iconSet,
+        emptyStar,
+        emptyStarColor,
+        fullStar,
+        halfStar,
+        starColor,
+        starSize,
+        starStyle,
+        style
+    } = this.props;
+
+    var starsLeft = this.state.rating;
+    const starButtons = [];
+
+    for (var i = 0, len = this.state.maxStars; i < len; i++) {
+      const Icon = iconSets[iconSet];
+      var starIconName = emptyStar;
 
       if (starsLeft >= 1) {
-        starIconName = this.props.fullStar;
-        starColor = this.props.starColor;
+        starIconName = fullStar;
       } else if (starsLeft === 0.5) {
-        starIconName = this.props.halfStar;
-        starColor = this.props.starColor;
+        starIconName = halfStar;
+      } else {
+        starColor = emptyStarColor;
       }
 
-      starButtons.push(
-        <StarButton
-          activeOpacity={0.20}
-          disabled={this.props.disabled}
-          key={i}
-          rating={i + 1}
-          onStarButtonPress={this.onStarButtonPress}
-          iconSet={this.props.iconSet}
-          starSize={this.props.starSize}
-          starIconName={starIconName}
-          starColor={starColor}
-        />
-      );
+      if (!this.props.disabled) {
+          starButtons.push(
+            <Button
+              activeOpacity={0.20}
+              disabled={disabled}
+              key={i + 1}
+              onPress={this.pressStarButton.bind(this, i + 1)}
+              style={{
+                height: starSize,
+                width: starSize,
+              }}
+            >
+              <Icon
+                name={starIconName}
+                size={starSize}
+                color={starColor}
+              />
+            </Button>
+          );
+      } else {
+          starButtons.push(
+              <Icon
+                key={i + 1}
+                name={starIconName}
+                size={starSize}
+                color={starColor}
+                style={starStyle}
+              />
+          );
+      }
       starsLeft--;
     }
 
     return (
-      <View style={styles.starRatingContainer}>
+      <View style={[styles.starRatingContainer, style]}>
         {starButtons}
       </View>
     );
   }
-}
+};
 
 StarRating.propTypes = {
+  style:  View.propTypes.style,
+  starStyle: Text.propTypes.style,
   disabled: PropTypes.bool,
   emptyStar: PropTypes.string,
   fullStar: PropTypes.string,
@@ -88,11 +147,11 @@ StarRating.propTypes = {
   iconSet: PropTypes.string,
   maxStars: PropTypes.number,
   rating: PropTypes.number,
-  selectedStar: PropTypes.func.isRequired,
+  selectedStar: PropTypes.func,
   starColor: PropTypes.string,
   emptyStarColor: PropTypes.string,
-  starSize: PropTypes.number,
-};
+  starSize: PropTypes.number
+}
 
 StarRating.defaultProps = {
   disabled: false,
@@ -104,7 +163,14 @@ StarRating.defaultProps = {
   rating: 0,
   starColor: 'black',
   emptyStarColor: 'gray',
-  starSize: 40,
-};
+  starSize: 40
+}
+
+const styles = StyleSheet.create({
+  starRatingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+});
 
 export default StarRating;
